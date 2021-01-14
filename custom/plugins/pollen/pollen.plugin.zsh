222bf2639a13b9299551a6ee85fd38f60e8e8275
Max Clayton Clowes <contact@mcclowes.com>
2021-01-14 12:30:50 +0000

    [feat] update

--- custom/plugins/pollen/pollen.plugin.zsh
+++ custom/plugins/pollen/pollen.plugin.zsh
@@ -7,12 +7,12 @@ function pollen-server-up-pollen {
   bin/server ambassador_api ambassador_api_worker phoenix_ui notification_service gateway_api pollen_ui avocado_ui apollo
 }
 
-function pollen-server-up-phoenix {
-  bin/server ambassador_api ambassador_api_worker phoenix_ui notification_service gateway_api           
+function pollen-server-up-pollen {
+  bin/server ambassador_api ambassador_api_worker phoenix_ui notification_service gateway_api pollen_ui apollo
 }
 
-function pollen-server-up-pollen {
-  pollen-server-up
+function pollen-server-up-phoenix {
+  bin/server ambassador_api ambassador_api_worker phoenix_ui notification_service gateway_api           
 }
 
 function pollen-server-up-admin {
