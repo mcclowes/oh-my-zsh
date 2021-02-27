8037afe86ece22005aa65453d3207cadd52f5f24
Max Clayton Clowes <contact@mcclowes.com>
2021-02-27 20:54:57 +0000

    [feat] rename files based on creation date

--- custom/plugins/experiments/experiments.plugin.zsh
+++ custom/plugins/experiments/experiments.plugin.zsh
@@ -5,6 +5,16 @@ function experimentsplit {
   echo ${ARR[2]};
 }
 
-function mvImg {
+# Make folder x
+# Move all files that match y -> x
+function mvImgToFolder {
   mkdir ${2} && mv ${1}* ./${2}   
+}
+
+
+function renameImgByDate {
+  for f in *.*; do
+    fn=$(basename "$f")
+    mv "$fn" "$(date -r "$f" +"%Y-%m-%d_%H-%M-%S")_$fn"
+  done
 }
